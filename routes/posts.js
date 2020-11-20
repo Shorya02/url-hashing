7687ad888dce52eeba40bd2c2af5223f8c1d8862
Shorya Choudhary <shorya@Shoryas-MacBook-Air.local>
2020-11-20 20:41:05 +0530

    minor changes

--- routes/posts.js
+++ routes/posts.js
@@ -3,18 +3,17 @@ const router = express.Router();
 
 //ROUTES
 router.get('/', (req, res) => {
-    console.log('Get Request Body ' + req.body);
-    console.log('Get Response Body ' + res.body);
     res.send('We are on posts');
 });
 
-router.get('/Specific', (req, res) => {
+/* router.get('/Specific', (req, res) => {
     res.send('Specifics');
-});
+}); */
 
-router.post('/ ', (req, res) => {
-    console.log('Post Request Body ' + req.body);
-    console.log('Post Response Body ' + res.body);
-})
+router.post('/', (req, res) => {
+    console.log('Request Body ' + req.body);
+    res.send('Received the Post Data');
+    console.log('Response Body ' + res);
+});
 
 module.exports = router;
